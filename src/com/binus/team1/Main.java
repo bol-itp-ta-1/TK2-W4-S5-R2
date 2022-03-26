2a2d925177442d819c4819e8fb27f0ef26dd9730
Luthfi Arifin <luthfiarifin0222@gmail.com>
2022-03-26 21:26:37 +0700

    Clean if statement

--- src/com/binus/team1/Main.java
+++ src/com/binus/team1/Main.java
@@ -7,33 +7,34 @@ public class Main {
         String name, nim;
         int number;
 
-        Scanner keyboard = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        System.out.print("Masukkan Nama Anda: ");
-        name = keyboard.nextLine();
+        System.out.print("Masukkan nama Anda: ");
+        name = input.nextLine();
 
-        if (name.length() <= 25) {
-            System.out.print("Masukkan NIM Anda (1o Digit): ");
-            nim = keyboard.nextLine();
-
-            if (nim.length() == 10) {
-                System.out.println("##########################################################");
-                System.out.println("Selamat datang " + name + " [NIM:" + nim + "]");
-                System.out.println("##########################################################");
-                System.out.print("Silakan pilih 1 angka antara 5-20: ");
-
-                number = keyboard.nextInt();
-
-                if (number >= 5 & (number <= 20)) {
-                } else {
-                    System.out.print("Masukkan Angka yang valid!");
-                }
-            } else {
-                System.out.print("Masukkan NIM yang valid!");
-            }
-        } else {
-            System.out.println("Maaf, Nama Anda Terlalu Panjang");
+        if (name.length() > 25) {
+            System.out.println("\nMaaf, nama Anda terlalu panjang, maksimum 25 karakter!");
+            return;
         }
 
+        System.out.print("Masukkan NIM Anda (10 Digit): ");
+        nim = input.nextLine();
+
+        if (nim.length() != 10) {
+            System.out.print("\nMaaf, NIM yang Anda masukkan tidak valid!");
+            return;
+        }
+
+        System.out.println("\n##########################################################");
+        System.out.println("Selamat datang " + name + " [NIM: " + nim + "]");
+        System.out.println("##########################################################\n");
+        System.out.print("Silakan pilih 1 angka antara 5-20: ");
+
+        number = input.nextInt();
+
+        if (number < 5 || number > 20) {
+            System.out.print("\nMasukkan angka yang valid!");
+            return;
+        }
     }
 }
